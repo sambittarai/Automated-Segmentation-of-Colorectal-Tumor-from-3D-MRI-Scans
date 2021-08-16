70ee22e0292052620f96e752e25569e318e53bbf
Sambit Tarai <sambitarai17@gmail.com>
2021-08-16 13:31:00 +0530

    preprocessing file added

---
+++ Libraries/pre_process.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+
+def my_PreProc(image, mask):
+    image = np.expand_dims(image, axis=1)
+    mask = np.expand_dims(mask, axis=1)
+    image = image/np.max(image)
+    mask = mask/255
+    return image, mask
+
+def my_PreProc_new(image, mask):
+    image = np.expand_dims(image, axis=1)
+    mask = np.expand_dims(mask, axis=1)
+    
+    image = dataset_normalized(image)
+    image = clahe_equalized(image)
+    mask = mask/255
+    return image, mask
+
+def dataset_normalized(imgs):
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[1]==1)  #check the channel is 1
+    imgs_normalized = np.empty(imgs.shape)
+    imgs_std = np.std(imgs)
+    imgs_mean = np.mean(imgs)
+    imgs_normalized = (imgs-imgs_mean)/imgs_std
+    for i in range(imgs.shape[0]):
+        imgs_normalized[i] = ((imgs_normalized[i] - np.min(imgs_normalized[i])) / (np.max(imgs_normalized[i])-np.min(imgs_normalized[i])))*255
+    return imgs_normalized
+
+def clahe_equalized(imgs):
+    """
+    adaptive histogram equalization is used. In this, image is divided into small blocks called "tiles" (tileSize is 8x8 by default in OpenCV). Then each of these blocks are histogram equalized as usual. So in a small area, histogram would confine to a small region (unless there is noise). If noise is there, it will be amplified. To avoid this, contrast limiting is applied. If any histogram bin is above the specified contrast limit (by default 40 in OpenCV), those pixels are clipped and distributed uniformly to other bins before applying histogram equalization. After equalization, to remove artifacts in tile borders, bilinear interpolation is applied.
+    """
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[1]==1)  #check the channel is 1
+    #create a CLAHE object (Arguments are optional).
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    imgs_equalized = np.empty(imgs.shape)
+    for i in range(imgs.shape[0]):
+        imgs_equalized[i,0] = clahe.apply(np.array(imgs[i,0], dtype = np.uint8))
+    return imgs_equalized
+
